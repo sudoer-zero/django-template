54001283ac1fc3a7753fcc38662c1eb609342766
Sudoer <sudoer.zero@gmail.com>
2022-06-27 06:12:52 +0200

    Add tests for Todo model.

--- apps/example/tests.py
+++ apps/example/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(
+            title="To-do example",
+            description="This is for testing",
+            done=False
+        )
+
+    def test_todo_title(self):
+        todo = Todo.objects.get(id=1)
+        field_label = todo._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_todo_description(self):
+        todo = Todo.objects.get(id=1)
+        field_label = todo._meta.get_field('description').verbose_name
+        self.assertEqual(field_label, 'description')
+
+    def test_todo_done(self):
+        todo = Todo.objects.get(id=1)
+        self.assertEqual(todo.done, False)
